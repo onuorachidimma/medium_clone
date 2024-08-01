fe2798947dddd648a496bb1f81ab505d948a36f2
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-08-01 04:02:19 +0100

    add hero/body

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,19 +1,26 @@
-import styles from './hero.module.css'
-import heroImage from '../assets/images/heroImage.webp'
+import styles from "./hero.module.css";
+import "../components/fonts.css";
+import heroImage from "../assets/images/heroImage.webp";
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      
-        <div>
-        <h1>Human stories & ideas</h1>
-        <p>A place to read, write, and deepen your understanding</p>
-        <a href="">Get Started</a>
+      <div className={styles.discount}>
+        <p>Be part of a better internet.</p>
+        <a href="">Get 20% off membership for a limited time</a>
+      </div>
+      <div className={styles.heroBody}>
+        <div className={styles.backgroundImage}>
+          <h2 className={styles.heroIntroText}>
+            Human <br /> stories & ideas
+          </h2>
+          <div className={styles.subTexts}>
+            <p>A place to read, write, and deepen your understanding</p>
+            <a className={styles.getStarted} href="">
+              Start reading
+            </a>
+          </div>
         </div>
-
-        <div>
-            <img src={heroImage} alt="" />
-        </div>
-      
+      </div>
     </div>
   );
 };
