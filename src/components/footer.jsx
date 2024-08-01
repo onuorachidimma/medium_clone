2c38815c575d75a94ff437246ff9c4f0cacd671d
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-08-01 04:29:01 +0100

    add footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,22 +1,20 @@
 import { Link } from "react-router-dom";
+import styles from './footer.module.css'
 const Footer = () => {
   return (
-    <div>
-      
-        <div>
-            <a href="#">Help</a>
-            <a href="#">Status</a>
-            <a href="#">About</a>
-            <a href="#">Career</a>
-            <a href="#">Press</a>
-            <a href="#">Blog</a>
-            <a href="#">Privacy</a>
-            <a href="#">Terms</a>
-            <a href="#">Text to spech</a>
-            <a href="#">Teams</a>
-            
-        </div>
-      
+    <div >
+      <div className={styles.footerContainer}>
+      <a href="#">Help</a>
+      <a href="#">Status</a>
+      <a href="#">About</a>
+      <a href="#">Careers</a>
+      <a href="#">Press</a>
+      <a href="#">Blog</a>
+      <a href="#">Privacy</a>
+      <a href="#">Terms</a>
+      <a href="#">Text to spech</a>
+      <a href="#">Terms</a>
+      </div>
     </div>
   );
 };
